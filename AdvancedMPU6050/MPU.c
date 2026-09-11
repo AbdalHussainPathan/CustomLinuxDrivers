5ad327546123dc5420eadde095ba6a1dddeb08c2
AbdalHussainPathan <abdalhussainpathan@gmail.com>
2026-09-11 21:16:44 +0200

    AdvancedMPU:Able to read sensor data Fully(Accel,Temp,Gyro)

---
+++ AdvancedMPU6050/MPU.c
@@ -0,0 +1,211 @@
+#include "MPU.h"
+static int mpu_read_reg(struct i2c_client *client,u8 reg)
+{
+	return i2c_smbus_read_byte_data(client,reg);
+}
+static int mpu_write_reg(struct i2c_client *client, u8 reg, u8 val)
+{
+	return i2c_smbus_write_byte_data(client, reg, val);
+}
+static int mpu_wakeup(struct i2c_client *client)
+{
+	/* PWR_MGMT_1: writing 0x00 clears the SLEEP bit and selects
+	 * the default clock source - takes the chip out of sleep mode. */
+	int ret=mpu_write_reg(client, MPU6050_REG_PWR_MGMT_1, 0x00);
+	if(ret<0)
+		return ret;
+	/* PWR_MGMT_2: bring all accel/gyro axes out of standby.
+	 * Chip was defaulting to 0x3F (all axes standby) on this board. */
+	ret = mpu_write_reg(client, MPU6050_REG_PWR_MGMT_2, 0x00);
+	if (ret < 0)
+		return ret;
+	msleep(50);
+	return 0;
+}
+/* Burst-read all 14 data registers in one I2C transaction */
+static int mpu_readburst(struct i2c_client *client, u8 *buf)
+{
+	int ret;
+
+	ret = i2c_smbus_read_i2c_block_data(client, MPU6050_REG_ACCEL_XOUT_H,MPU6050_BURST_LEN, buf);
+	if (ret < 0)
+		return ret;
+	if (ret != MPU6050_BURST_LEN)
+		return -EIO;
+	return 0;
+}
+static void Decode_MPU(struct mpu6050_sample *sample, uint8_t *data)
+{
+	sample->accel_x=(s16)((data[0]<<8)|data[1]);
+	sample->accel_y=(s16)((data[2]<<8)|data[3]);
+	sample->accel_z=(s16)((data[4]<<8)|data[5]);
+	sample->temp_raw=(s16)((data[6]<<8)|data[7]);
+	sample->gyro_x=(s16)((data[8]<<8)|data[9]);
+	sample->gyro_y=(s16)((data[10]<<8)|data[11]);
+	sample->gyro_z=(s16)((data[12]<<8)|data[13]);
+}
+static int close_mpu (struct inode *inode, struct file *filp)
+{
+   pr_info("close was success\n");
+  return 0;
+}
+static int open_mpu(struct inode *inode, struct file *filp)
+{
+  pr_info("mpu6050 : Open\n");
+  filp->f_pos=0;
+  return 0;
+}
+static ssize_t write_mpu(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
+{
+  pr_info("write is called\n");
+  return count;
+}
+static ssize_t read_mpu(struct file *filp, char __user *buff, size_t count, loff_t *f_pos)
+{
+  struct Mpu_I2cDev *mpu=pI2cMpu_Handle;
+  u8 Raw_data[MPU6050_BURST_LEN];
+  struct mpu6050_sample read_sample;
+  int ret=0;
+  size_t to_copybytes;
+
+  pr_info("read is called\n");
+
+  if(!mpu||!mpu->client)
+  	{return -ENODEV;}
+  if(*f_pos!=0)
+  	{return 0;}
+  mutex_lock(&mpu->lock);
+  ret=mpu_readburst(mpu->client,Raw_data);
+  if (ret < 0) 
+  {
+	mutex_unlock(&mpu->lock);
+	pr_err("mpu6050: burst read failed (%d)\n", ret);
+	return ret;
+  }
+  Decode_MPU(&read_sample,Raw_data);
+  
+  mpu->msg_len=scnprintf(mpu->msg,sizeof(mpu->msg),
+  "ax=%d ay=%d az=%d temp_raw=%d gx=%d gy=%d gz=%d\n",
+  read_sample.accel_x,read_sample.accel_y,read_sample.accel_z,
+  read_sample.temp_raw,read_sample.gyro_x,read_sample.gyro_y,read_sample.gyro_z);
+  
+  to_copybytes=min(count,mpu->msg_len);
+  
+  if(copy_to_user(buff,mpu->msg,to_copybytes))
+  {
+  	mutex_unlock(&mpu->lock);
+	return -EFAULT;
+  }
+  mutex_unlock(&mpu->lock);
+  *f_pos += to_copybytes;
+  return to_copybytes;
+}
+int I2C_init(struct Mpu_I2cDev *mpu)
+{
+    int ret=0;
+    ret=alloc_chrdev_region(&mpu->dev_num,0,1,"MPU");
+    if(ret<0)
+        return ret;
+    cdev_init(&mpu->cdev_mpu,&fops);
+    if(cdev_add(&mpu->cdev_mpu,mpu->dev_num,1)<0)
+        goto unreg_dev;
+    mpu->class_mpu=class_create("Class_MPU");
+    if(IS_ERR(mpu->class_mpu))
+        goto cdev_del;
+    mpu->device_mpu=device_create(mpu->class_mpu,NULL,mpu->dev_num,NULL,"MPU_6050");
+    if(IS_ERR(mpu->device_mpu))
+        goto class_destroy;
+    pr_info("Major %d : Minor %d",MAJOR(mpu->dev_num),MINOR(mpu->dev_num));
+
+    return 0;
+class_destroy:
+    class_destroy(mpu->class_mpu);
+cdev_del:
+    cdev_del(&mpu->cdev_mpu); 
+unreg_dev:
+    unregister_chrdev_region(mpu->dev_num,1);
+    return -1;
+}
+void I2C_exit(struct Mpu_I2cDev *mpu)
+{
+    device_destroy(mpu->class_mpu,mpu->dev_num);
+    unregister_chrdev_region(mpu->dev_num,1);
+    cdev_del(&mpu->cdev_mpu);
+    class_destroy(mpu->class_mpu);
+}
+static int mpu_probe(struct i2c_client *client)
+{
+	struct Mpu_I2cDev *pMpu_Dev;
+	int ret=0;
+	int nWhoami=mpu_read_reg(client,MPU6050_REG_WHO_AM_I);
+	if(nWhoami<0)
+	{
+	   return nWhoami;
+	}
+	if (nWhoami != MPU6050_WHOAMI_MPU6050 && nWhoami != MPU6050_WHOAMI_MPU6500)
+	{
+		dev_err(&client->dev, "Expected 0x68 or 0x70 instead got 0x%02x", nWhoami);
+		return -ENODEV;
+	}
+	pMpu_Dev=devm_kzalloc(&client->dev,sizeof(*pMpu_Dev),GFP_KERNEL);
+	if(pMpu_Dev==NULL)
+	{
+		return -ENOMEM;
+	}
+	pMpu_Dev->client=client;
+	mutex_init(&pMpu_Dev->lock);
+	i2c_set_clientdata(client,pMpu_Dev);
+	pI2cMpu_Handle=pMpu_Dev;
+	ret=mpu_wakeup(client);
+	if(ret<0)
+	{
+		dev_err(&client->dev, "failed to wake device (%d)\n", ret);
+		return ret;
+	}
+	ret=I2C_init(pMpu_Dev);
+	if(ret<0)
+	{
+		dev_err(&client->dev, "failed to init char device (%d)\n", ret);
+		return ret;
+	}
+	dev_info(&client->dev, "mpu6050 driver probe complete\n");
+	return ret;	
+}
+static void mpu_remove(struct i2c_client *client)
+{
+	struct Mpu_I2cDev *mpu=i2c_get_clientdata(client);
+	if(mpu)
+	{
+		I2C_exit(mpu);
+		if(mpu==pI2cMpu_Handle)
+			pI2cMpu_Handle=NULL;
+	}
+	dev_info(&client->dev, "mpu6050 driver removed\n");
+}
+/*
+ * Matches when a client is created with this name - either via
+ * manual sysfs new_device echo ("mpu6050 0x68") or a DT node with
+ * compatible = "invensense,mpu6050" (once you add the of_match_table).
+ */
+static const struct i2c_device_id mpu6050_id[] = {
+	{ "mpu6050", 0 },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, mpu6050_id);
+static struct i2c_driver mpu_driver=
+{
+	.driver=
+	{
+	  .name=DRIVER_NAME
+	},
+	.probe=mpu_probe,
+	.remove=mpu_remove,
+	.id_table=mpu6050_id
+};
+
+module_i2c_driver(mpu_driver);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ABDAL");
+MODULE_DESCRIPTION("MPU6050 Char Driver");
+MODULE_INFO(board,"Prog for BeagleBone");
---
+++ AdvancedMPU6050/MPU.h
@@ -0,0 +1,63 @@
+#include<linux/module.h>
+#include<linux/device.h>
+#include<linux/cdev.h>
+#include<linux/kdev_t.h>
+#include<linux/uaccess.h>
+#include <linux/i2c.h>
+#include <linux/delay.h>
+#include <linux/mutex.h>
+#include <linux/version.h>
+#define DRIVER_NAME			       "mpu6050"
+#define MPU6050_REG_PWR_MGMT_1      0x6B
+#define MPU6050_REG_WHO_AM_I		0x75
+#define MPU6050_REG_ACCEL_XOUT_H	0x3B	/* first of 14 consecutive data regs */
+#define MPU6050_BURST_LEN		    14	/* accel(6) + temp(2) + gyro(6) */
+#define MPU6050_WHOAMI_MPU6050      0x68
+#define MPU6050_WHOAMI_MPU6500      0x70
+#define MPU6050_REG_PWR_MGMT_2		0x6C
+//Device
+struct Mpu_I2cDev
+{
+	char msg[256];
+	size_t msg_len;
+	struct mutex lock;
+	dev_t dev_num;
+	struct cdev cdev_mpu;
+	struct class *class_mpu;
+	struct device *device_mpu;
+	struct i2c_client *client;
+};
+struct Mpu_I2cDev *pI2cMpu_Handle;
+
+//Functions
+int I2C_init(struct Mpu_I2cDev *mpu);
+void I2C_exit(struct Mpu_I2cDev *mpu);
+
+static int mpu_probe(struct i2c_client *client);
+static void mpu_remove(struct i2c_client *client);
+
+static int open_mpu(struct inode *inode,struct file *filp);
+static int close_mpu(struct inode *inode,struct file *filp);
+static ssize_t write_mpu(struct file *filp,const char __user *buff,size_t count,loff_t *fpos);
+static ssize_t read_mpu(struct file *filp, char __user *buff,size_t count,loff_t *fpos);
+
+struct file_operations fops=
+{
+    .read=read_mpu,
+    .write=write_mpu,
+    .release=close_mpu,
+    .open=open_mpu,
+    .owner=THIS_MODULE
+};
+
+//MPU Helpers
+struct mpu6050_sample {
+	s16 accel_x, accel_y, accel_z;
+	s16 temp_raw;
+	s16 gyro_x, gyro_y, gyro_z;
+};
+static int mpu_readburst(struct i2c_client *client, u8 *buf);
+static int mpu_read_reg(struct i2c_client *client,u8 reg);
+static int mpu_wakeup(struct i2c_client *client);
+static int mpu_write_reg(struct i2c_client *client, u8 reg, u8 val);
+static void Decode_MPU(struct mpu6050_sample *sample, uint8_t *data);
